1acd0d91d6b961be5a2456909f60bd8ef2267357
Pavel <93835948+pkorytkin@users.noreply.github.com>
2022-09-08 23:16:54 +0300

    Stable

--- src/topic_publisher.py
+++ src/topic_publisher.py
@@ -2,21 +2,26 @@
 # -*- coding: utf-8 -*-
 import math
 import rospy
-#from std_msgs.msg import String
 import geometry_msgs.msg
 import turtlesim.msg
 import math
-#CurrentPosition=turtlesim.msg.Pose
 
 StartPoseSaved=False
+#Глобальная переменная для нынешней позиции
 CurrentPose=turtlesim.msg.Pose()
+#Сохранённая стартовая позиция
 StartPose=turtlesim.msg.Pose()
+#Сообщение поворота отправляемое в Topic
 twist=geometry_msgs.msg.Twist()
 #Целевая точка маршрута
 CurrentPointID=0
+#Сохранённые координаты точек маршрута
 PointList=[]
+#Rate
 r=any
+#Число координат выпуклого многоугольника
 PointsCount=20
+
 class Vector3:
     x=0
     y=0
@@ -40,40 +45,38 @@ class Vector3:
         return "x:"+str(self.x)+" y:"+str(self.y)+" z:"+str(self.z)
     def convertToMSG(self):
         return geometry_msgs.msg.Vector3(self.x,self.y,self.z)
+    def Module(self):
+        return float(math.sqrt((self.x)**2+(self.y)**2+(self.z)**2))
 
-def ModuleVector3(Vector:Vector3):
-    return float(math.sqrt((Vector.x)**2+(Vector.y)**2+(Vector.z)**2))
 def DistanceVector3(fromPosition:Vector3,toPosition:Vector3=Vector3()):
-    return ModuleVector3(fromPosition-toPosition)
+    return (fromPosition-toPosition).Module()
 def Angle3D(fromVector:Vector3,toVector:Vector3):
     #https://www.wikihow.com/Find-the-Angle-Between-Two-Vectors
     print(fromVector)
     print(toVector)
-    #print(str(DistanceVector3(fromVector))+" "+str(DistanceVector3(toVector)))
-    #print("Angle="+str(math.acos(fromVector*toVector/(DistanceVector3(fromVector)*DistanceVector3(toVector))).real))
     sign=1
     if (fromVector.x*toVector.y-fromVector.y*toVector.x<0):
         sign=-1
     return sign*math.acos(fromVector*toVector/(DistanceVector3(fromVector)*DistanceVector3(toVector))).real
-    #dot=fromVector.x*toVector.x+fromVector.y+toVector.y
-    #det=fromVector.x*toVector.y-fromVector.y*toVector.x
-    #angle=math.atan2(det,dot)
-    #return angle
+#Преобразование локальной позиции черепахи в мировую
 def LocalPositionToWorld(Vector:Vector3):
     return CurrentPosition()+Vector
-
+#Мировая позиция в локальную
 def WorldToLocalPosition(Vector:Vector3):
     return Vector-CurrentPosition()
-
+#Нынешняя позция
 def CurrentPosition():
     #z=CurrentPose.z
     position=Vector3(x=CurrentPose.x,y=CurrentPose.y)
     return position
+#Нынешняя ротация в углах эйлера
 def CurrentRotation():
     rotation=Vector3(x=0,y=0,z=CurrentPose.theta)
     return rotation
+#Нынешней глобальный вектор вперёд
 def CurrentGlobalForward():
     return Vector3(x=math.cos(CurrentPose.theta).real,y=math.sin(CurrentPose.theta).real,z=0)
+#Локальный вектор вперёд x=1
 def CurrentLocalForward():
     return Vector3(1,0,0)
 
@@ -86,7 +89,6 @@ def subscriber_pose(pose:turtlesim.msg.Pose):
     if(not StartPoseSaved):
         StartPose=pose
         StartPoseSaved=True
-        pass
     #Сохранение нынешней позиции
     
     CurrentPose=pose
@@ -141,15 +143,9 @@ def Worker():
         twist.angular=Vector3(0,0,0)
         twist.linear=Vector3()
         print("DistanceToPoint="+str(DistanceToPoint))
-        #pub.publish(twist)
-        #continue
     
         if(abs(AngleFromForwardToPoint)>0.03):
             twist.angular.z=AngleFromForwardToPoint
-            """if(AngleFromForwardToPoint>0):
-                twist.angular.z=AngleFromForwardToPoint
-            else:
-                twist.angular.z=AngleFromForwardToPoint"""
         else:
             if(DistanceToPoint>0.01):
                 twist.linear=(CurrentLocalForward()*max(DistanceToPoint,0.005)).convertToMSG()
